8f4c3abc0b5f0014d3abfcfe38feb3c8ee14f080
Kyle Shanks <kyle.j.shanks@gmail.com>
2018-08-13 14:50:09 -0400

    Initialize repository and add first project

---
+++ first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    render json: User.find_by(id: params[:id])
+  end
+
+  def update
+    user = User.find_by(id: params[:id])
+    if user
+      user.update(user_params)
+      render json: user
+    else
+      render plain: 'User not found', status: 404
+    end
+  end
+
+  def delete
+    user = User.find_by(id: params[:id])
+    if user
+      user.destroy
+      render plain: 'User destroyed'
+    else
+      render plain: 'User not found', status: 404
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
